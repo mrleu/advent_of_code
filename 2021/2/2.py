3f9ecb5712d8a8b46089f9ca5e5ca45a7c18d1fe
Alex Leu <alexcleu@ucla.edu>
2021-12-03 00:26:07 -0800

    updated

--- 2021/2/2.py
+++ 2021/2/2.py
@@ -6,16 +6,16 @@ down 8
 forward 2"""
 
 # part 1
-files = open('input.txt').read().splitlines()
-parsed = [x.split(' ') for x in files]
+files = open("input.txt").read().splitlines()
+parsed = [x.split(" ") for x in files]
 start = [0, 0]
 for (d, idx) in parsed:
     idx = int(idx)
-    if d == 'forward':
+    if d == "forward":
         start[0] += idx
-    elif d == 'down':
+    elif d == "down":
         start[1] += idx
-    elif d == 'up':
+    elif d == "up":
         start[1] -= idx
 print(f"Solution for part 1: {start[0] * start[1]}")
 
@@ -23,32 +23,39 @@ print(f"Solution for part 1: {start[0] * start[1]}")
 start = [0, 0, 0]
 for (d, idx) in parsed:
     idx = int(idx)
-    if d == 'forward':
+    if d == "forward":
         start[0] += idx
-        start[1] += (start[2] * idx)
-    elif d == 'down':
+        start[1] += start[2] * idx
+    elif d == "down":
         start[2] += idx
-    elif d == 'up':
+    elif d == "up":
         start[2] -= idx
 print(f"Solution for part 2: {start[0] * start[1]}")
 
 # pandas part 1
 import pandas as pd
-df = pd.read_csv('input.txt', names=['direction', 'steps'], sep=' ')
-df['steps'] = df.apply(lambda x: -x['steps'] if x['direction'] =='up' else x['steps'], axis=1)
-solution = df[df.direction =='forward'].steps.sum()  * df[~(df.direction == 'forward')].steps.sum()
+
+df = pd.read_csv("input.txt", names=["direction", "steps"], sep=" ")
+df["steps"] = df.apply(
+    lambda x: -x["steps"] if x["direction"] == "up" else x["steps"], axis=1
+)
+solution = (
+    df[df.direction == "forward"].steps.sum()
+    * df[~(df.direction == "forward")].steps.sum()
+)
 print(f"Solution for part 1 in pandas: {solution}")
 
 # pandas part 2
 import pandas as pd
 from functools import reduce
 import operator
-df = pd.read_csv('input.txt', names=['direction', 'size'], sep=' ')
-mapping_dict = {'down':1,'up':-1}
-df['sign'] = df['direction'].map(mapping_dict)
-df['aim'] = (df['size'] * df['sign']).fillna(0)
-df['aim_sum'] = df['aim'].cumsum()
-df['depth'] = df['aim_sum'] * df['size']
-out = df.query('direction == "forward"')[['size','depth']].sum()
+
+df = pd.read_csv("input.txt", names=["direction", "size"], sep=" ")
+mapping_dict = {"down": 1, "up": -1}
+df["sign"] = df["direction"].map(mapping_dict)
+df["aim"] = (df["size"] * df["sign"]).fillna(0)
+df["aim_sum"] = df["aim"].cumsum()
+df["depth"] = df["aim_sum"] * df["size"]
+out = df.query('direction == "forward"')[["size", "depth"]].sum()
 solution = reduce(operator.mul, out.tolist())
 print(f"Solution for part 2 in pandas: {solution}")
