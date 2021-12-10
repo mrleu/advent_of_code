60ba2f73c210d3ec6d16feaaee183510ad518b55
Alex Leu <alexcleu@ucla.edu>
2021-12-09 21:58:15 -0800

    clean code

--- 2021/10/day_10.py
+++ 2021/10/day_10.py
@@ -3,33 +3,33 @@ with open("input.txt", "r") as f:
 
 corrupted = []
 incompletes = []
-mapping = {"}": "{", ")": "(", ">": "<", "]": "["}
-rev_map = {v: k for k, v in mapping.items()}
-points_map = {")": 3, "]": 57, "}": 1197, ">": 25137}
-open_map = {")": 1, "]": 2, "}": 3, ">": 4}
+close_open_map = {"}": "{", ")": "(", ">": "<", "]": "["}
+open_close_map = {v: k for k, v in close_open_map.items()}
+corrupted_character_points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+incomplete_character_points = {")": 1, "]": 2, "}": 3, ">": 4}
 
 for line in raw.split("\n"):
     queue = []
     broken = False
     for char in line:
-        if char in ["[", "(", "<", "{"]:
+        if char in open_close_map.keys():
             queue.append(char)
         else:
             take_out = queue.pop()
-            if take_out != mapping[char]:
+            if take_out != close_open_map[char]:
                 corrupted.append(char)
                 broken = True
     if not broken:
-        incompletes.append(list(reversed([rev_map[x] for x in queue])))
+        incompletes.append(list(reversed([open_close_map[x] for x in queue])))
 
-print("Part 1 score:", sum([points_map[char] for char in corrupted]))
+print("Part 1 score:", sum([corrupted_character_points[char] for char in corrupted]))
+
 scores = []
 for incomplete in incompletes:
-    n = 0
+    score = 0
     for char in incomplete:
-        n *= 5
-        n += open_map[char]
-    scores.append(n)
-
+        score *= 5
+        score += incomplete_character_points[char]
+    scores.append(score)
 sorted_scores = sorted(scores)
 print("Part 2 score:", sorted_scores[int((len(scores) - 1) / 2)])
