cbe7e14fa68d9e237b6fbd6b520d6735a21b7ca1
Alex Leu <alexcleu@ucla.edu>
2021-12-15 11:35:56 -0800

    clean by black

--- 2021/15/day_15.py
+++ 2021/15/day_15.py
@@ -7,49 +7,41 @@ def read_data():
         return [list(map(int, list(x))) for x in raw.strip().split("\n")]
 
 
-def five_times(data):
-    n_row = len(data)
-    n_col = len(data[0])
+def dijkstra(data, five_times):
+    multiple = 5 if five_times else 1
+    nrows = len(data) * multiple
+    ncols = len(data[0]) * multiple
 
-    for row in data:
-        for chunk in range(4):
-            row += [((col + 1) % 10) or 1 for col in row[n_col * chunk :]]
+    def risk_level(i, j):
+        row = len(data)
+        col = len(data[0])
+        risk = (i // row) + (j // col + data[i % row][j % col]) % 9
+        return 9 if risk == 0 else risk
 
-    for chunk in range(4):
-        for row in data[n_row * chunk :]:
-            data.append([((col + 1) % 10) or 1 for col in row])
-    return data
-
-
-def dijkstra(data):
-    distances = {
-        (c, r): float("inf") for r in range(len(data)) for c in range(len(data[0]))
-    }
+    distances = {(c, r): float("inf") for r in range(nrows) for c in range(ncols)}
     start = (0, 0)
     distances[start] = 0
 
     heap = [(0, start)]
-
     while heap:
         distance, u = heapq.heappop(heap)
-        for dr, dc in ((0, 1), (1, 0), (-1, 0), (0, -1)):
-            nr, nc = u[0] + dr, u[1] + dc
-            if 0 <= nr < len(data) and 0 <= nc < len(data[0]):
-                neighbor = (nr, nc)
-                if (new_dist := distance + data[nr][nc]) < distances[neighbor]:
+        for delta in ((0, 1), (1, 0), (-1, 0), (0, -1)):
+            neighbor = tuple(map(sum, zip(u, delta)))
+            if neighbor in distances:
+                if (new_dist := distance + risk_level(*neighbor)) < distances[neighbor]:
                     distances[neighbor] = new_dist
                     heapq.heappush(heap, (new_dist, neighbor))
-    print(distances[(len(data[0]) - 1, len(data) - 1)])
+
+    print(distances[(nrows - 1, ncols - 1)])
 
 
 def main():
     data = read_data()
     print("=" * 10, "part 1", "=" * 10)
-    dijkstra(data)
+    dijkstra(data, five_times=False)
 
-    five_times_data = five_times(data)
     print("=" * 10, "part 2", "=" * 10)
-    dijkstra(five_times_data)
+    dijkstra(data, five_times=True)
 
 
 if __name__ == "__main__":
