1d8424da58d5ee0f1cae5f8742ea8a540fd35877
Alex Leu <alexcleu@ucla.edu>
2021-12-16 22:32:43 -0800

    adagian

--- 2021/17/day_17.py
+++ 2021/17/day_17.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import re
 
 
@@ -9,47 +10,52 @@ def parse_data(data_input):
 def in_box(coordinate, target_area):
     target_x, target_y = target_area
     if (
-        target_x[0] <= coordinate[0] <= target_x[1]
-        and target_y[0] <= coordinate[1] <= target_y[1]
+        target_x[0] <= coordinate.x <= target_x[1]
+        and target_y[0] <= coordinate.y <= target_y[1]
     ):
         return True
     return False
 
 
-def trickshot(x, y, target_area):
+@dataclass
+class Coordinate:
+    x: int
+    y: int
+
+
+def trickshot(x_grad, y_grad, target_area):
     furthest = target_area[1][0]
-    coordinate = [0, 0]
+    coordinate = Coordinate(0, 0)
     ys = []
-    while coordinate[1] >= furthest:
-        # print("x, y", x, y, "coordinate", coordinate)
-        coordinate[0] += x
-        coordinate[1] += y
-        ys.append(coordinate[1])
+    while coordinate.y >= furthest:
+        coordinate.x += x_grad
+        coordinate.y += y_grad
+
+        ys.append(coordinate.y)
         if in_box(coordinate, target_area):
-            # print(f"Hit in the coordinate! at {coordinate}, with highest y at {max(ys)}")
-            return (True, max(ys))
-            break
-        if x < 0:
-            x += 1
-        elif x == 0:
-            x = 0
-        elif x > 0:
-            x -= 1
-        y -= 1
-        if coordinate[1] < furthest:
-            # print("sorry, didn't hit")
-            return (False, None)
+            return True, max(ys)
+
+        if x_grad < 0:
+            x_grad += 1
+        elif x_grad == 0:
+            x_grad = 0
+        elif x_grad > 0:
+            x_grad -= 1
+
+        y_grad -= 1
+
+        if coordinate.y < furthest:
+            return False, None
 
 
 def grid_search(target_area):
     works = set()
     highest = []
-    for x in range(-200, 200):
-        for y in range(-200, 200):
-            made_it, high_y = trickshot(x, y, target_area)
+    for x_grad in range(-200, 200):
+        for y_grad in range(-200, 200):
+            made_it, high_y = trickshot(x_grad, y_grad, target_area)
             if made_it:
-                # print("Velocity that works:", (x, y))
-                works.add((x, y))
+                works.add((x_grad, y_grad))
                 highest.append(high_y)
     print("Highest position:", max(highest))
     print("Unique velocity", len(works))
